cb60a6f039119ace68f459d5455ac53a878ec8e5
inonvv <115711486+inonvv@users.noreply.github.com>
2023-12-04 00:20:19 +0200

    arithmatic btn transform animation

--- src/componnets/Arithmatic/Arithmatic.jsx
+++ src/componnets/Arithmatic/Arithmatic.jsx
@@ -1,18 +1,19 @@
+import React, { useState } from "react";
 import { Button } from "../Button/Button";
+import { useSpring, animated } from "@react-spring/web";
+
 import "./Arithmatic.css";
 export const Arithmatic = ({ setOpertor, setEquals, clear, clearHistory }) => {
   const [animationStates, setAnimationStates] = useState({
-    one: false,
-    two: false,
-    three: false,
+    plus: false,
+    minus: false,
+    multiply: false,
+    division: false,
+    equlas: false,
+    clear: false,
   });
-  const handleNumberClick = (num, button) => {
-    if (opertor !== undefined) {
-      setNumber2((prev) => (prev ? prev * 10 + num : num));
-    } else {
-      setNumber1((prev) => (prev ? prev * 10 + num : num));
-    }
 
+  const operatorBtnName = (button) => {
     setAnimationStates((prev) => ({
       ...prev,
       [button]: true,
@@ -26,69 +27,107 @@ export const Arithmatic = ({ setOpertor, setEquals, clear, clearHistory }) => {
     }, 200);
   };
 
-  const numberSprings = {
-    one: useSpring({
-      transform: animationStates.one ? "scale(1.10)" : "scale(1)",
+  const operatorSprings = {
+    plus: useSpring({
+      transform: animationStates.plus ? "scale(1.10)" : "scale(1)",
     }),
-    two: useSpring({
-      transform: animationStates.two ? "scale(1.10)" : "scale(1)",
+    minus: useSpring({
+      transform: animationStates.minus ? "scale(1.10)" : "scale(1)",
     }),
-    three: useSpring({
-      transform: animationStates.three ? "scale(1.10)" : "scale(1)",
+    multiply: useSpring({
+      transform: animationStates.multiply ? "scale(1.10)" : "scale(1)",
+    }),
+    division: useSpring({
+      transform: animationStates.division ? "scale(1.10)" : "scale(1)",
+    }),
+    equlas: useSpring({
+      transform: animationStates.equlas ? "scale(1.10)" : "scale(1)",
+    }),
+    clear: useSpring({
+      transform: animationStates.clear ? "scale(1.10)" : "scale(1)",
     }),
   };
 
   return (
     <div className="container">
       <div className="row">
-        <Button
-          onClick={() => {
-            setOpertor("+");
-          }}
-        >
-          +
-        </Button>
-        <Button
-          onClick={() => {
-            setOpertor("-");
-          }}
-        >
-          -
-        </Button>
-        <Button
-          onClick={() => {
-            setOpertor("*");
-          }}
-        >
-          *
-        </Button>
-        <Button
-          onClick={() => {
-            setOpertor("/");
-          }}
-        >
-          /
-        </Button>
+        <animated.div style={operatorSprings.plus}>
+          <Button
+            onClick={() => {
+              setOpertor("+");
+              operatorBtnName("plus");
+            }}
+          >
+            +
+          </Button>
+        </animated.div>
+        <animated.div>
+          <Button
+            style={operatorSprings.minus}
+            onClick={() => {
+              setOpertor("-");
+              operatorBtnName("minus");
+            }}
+          >
+            -
+          </Button>
+        </animated.div>
+        <animated.div style={operatorSprings.multiply}>
+          <Button
+            onClick={() => {
+              setOpertor("*");
+              operatorBtnName("multiply");
+            }}
+          >
+            *
+          </Button>
+        </animated.div>
+        <animated.div style={operatorSprings.division}>
+          <Button
+            onClick={() => {
+              setOpertor("/");
+              operatorBtnName("division");
+            }}
+          >
+            /
+          </Button>
+        </animated.div>
       </div>
       <div className="row">
-        <Button
-          onClick={() => {
-            setEquals(true);
-          }}
-        >
-          =
-        </Button>
-        <Button
-          onClick={() => {
-            clear();
-          }}
-        >
-          Ce
-        </Button>
+        <animated.div style={operatorSprings.equlas}>
+          <Button
+            onClick={() => {
+              setEquals(true);
+              operatorBtnName("equlas");
+            }}
+          >
+            =
+          </Button>
+        </animated.div>
+        <animated.div>
+          <Button
+            style={operatorSprings.clear}
+            onClick={() => {
+              clear();
+              operatorBtnName("clear");
+            }}
+          >
+            Ce
+          </Button>
+        </animated.div>
 
-        <Button onClick={clearHistory} className="clean-history">
-          clean history
-        </Button>
+        <animated.div style={operatorSprings.clear}>
+          {/* <Button onClick={clearHistory} */}
+          <Button
+            onClick={() => {
+              clearHistory();
+              operatorBtnName("clear");
+            }}
+            className="clean-history"
+          >
+            clean history
+          </Button>
+        </animated.div>
       </div>
     </div>
   );
